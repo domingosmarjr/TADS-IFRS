1adfe56661f0957faea5bedf54ddaf265cdcd864
Domingos Martins Jr <domingos.mar.jr@outlook.com>
2025-03-14 19:23:52 -0300

    Atualizei Pasta JS

---
+++ Disciplinas/Programacao-Modular/aula001-funcoes/index.js
@@ -0,0 +1,17 @@
+console.log('Ok');
+
+console.log(Math.random())
+
+//--- OBJETO MATH
+console.log(Math);
+// Potência (igual java)
+console.log(Math.pow(2,2));
+// Random
+console.log(Math.random)
+console.log(Math.random())
+
+// DOCUMENTAÇÃO DO JAVASCRIPT
+// Mozila -> responsável por manter a documentação do JS
+// Mozila Development Network - MDN
+
+
