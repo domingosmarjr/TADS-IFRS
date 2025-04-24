3975026e89bd6099e6e6ba1242bb9bf8dabe0b87
Domingos Martins Jr <domingos.mar.jr@outlook.com>
2025-04-24 11:14:38 -0300

    socorro

--- Disciplinas/Programacao-Modular/trabalho001/index.js
+++ Disciplinas/Programacao-Modular/trabalho001/index.js
@@ -35,33 +35,32 @@ try {
     console.error(e)
 }
 
-*/
 
 //                0123
 console.log(half('tads', 1)) // primeira metade: 'ta'
 console.log(half('tads', 2)) // segunda metade: 'ds'
-console.log(half('tads', 3)) // terceira metade: ''
-
+console.log(half('tads', 3) == '') // terceira metade: ''
 
 // REGRA GERAL, numa divisão desbalanceada, a primeria metade deve ser sempre menor que a segunda
 console.log(half('tecno', 1)) // 'te'
 console.log(half('tecno', 2)) // 'cno'
-
 console.log(half('tecno')) // 'te'
+console.log(half() == '') // ''
 
-console.log(half()) // ''
 
-
-/*
 // half deve aceitar números, sempre tratando como inteiros
 console.log(half(24)) // 12
 console.log(half(25)) // 12
 console.log(half(25, 1)) // 12
 console.log(half(25, 2)) // 13
+
+
 // se sobrarem decimais, ficam na segunda parte
 console.log(half(25.5, 1)) // 12
 console.log(half(25.5, 2)) // 13.5
-console.log(half(25.9, 2)) // 13.9
+console.log(half(25.9, 2)) // 13.9  
+*/
+
 
 // group é semelhante a half
 console.log(group('tecnologia', 3, 1)) // dividir em três partes e retornar a primeira
@@ -69,6 +68,7 @@ console.log(group('tecnologia', 3, 1)) // dividir em três partes e retornar a p
 console.log(group('tecnologia', 3, 1) == 'tec')
 console.log(group('tecnologia', 3, 2) == 'nol')
 console.log(group('tecnologia', 3, 3) == 'ogia')
+/*
 // parâmetros inválidos devolvem string vazia
 console.log(group('tecnologia', 3, 4) == '')
 console.log(group('tecnologia', 3, -1) == '')
@@ -80,7 +80,8 @@ try {
 } catch (e) {
     console.error(e)
 }
-
+*/
+/*
 // em 10 partes
 console.log(group('tecnologia', 10, 1) == 't')
 console.log(group('tecnologia', 10, 2) == 'e')
--- Disciplinas/Programacao-Modular/trabalho001/trabalho.js
+++ Disciplinas/Programacao-Modular/trabalho001/trabalho.js
@@ -52,25 +52,62 @@ export function trim(string) {
 }
 
 //HALF
-export function half(string = '', metade = 0) {
-    let nova = ''
+export function half(string = '', metade = 1) {
     
-    if (metade == 0) return string
-
-    if (string.length % 2 == 0) { //STRING PAR
-        if (metade == 1) nova = substring(string, 0, string.length/2)  
-        if (metade == 2) nova = substring(string, string.length/2, string.length)
-    } else if (string.length % 2 != 0) { //STRING IMPAR (precisa encontrar o char MEDIANA
-        let mediana = medianaString(string)
-        if (metade == 1) nova = substring(string, 0, string.length/2 - 1)
-        if (metade == 2) nova = substring(string, mediana)
+    let nova = ''
+        
+    if (typeof(string) == 'string') {
+        if (metade == 0) return string
+        if (string.length % 2 == 0) { //STRING PAR
+            if (metade == 1) nova = substring(string, 0, string.length/2)  
+            if (metade == 2) nova = substring(string, string.length/2, string.length)
+        } else if (string.length % 2 != 0) { //STRING IMPAR (precisa encontrar o char MEDIANA
+            let mediana = medianaString(string)
+            if (metade == 1) nova = substring(string, 0, string.length/2 - 1)
+            if (metade == 2) nova = substring(string, mediana)
+        }    
+    } else if (typeof(string) == 'number') {
+        let metadeInteiro = arrendonda(string/2)
+        let decimal = string % 1
+        let resultado = (metade == 1) ? metadeInteiro : 1 + metadeInteiro + decimal
+        // Tive que pesquisar para corrigir ponto flutuante de half(25.9,2)
+        // pois sempre estava resultando 13.8999 ao invés de 13.9
+        const precision = 1e10
+        return (parseInt(resultado * precision))/precision
     }
     return nova
 }
 
-export function group() {
+export function group(texto, divisor, parte) {
+
+    if (typeof(texto) != 'string') throw 'Texto inserido não é string.'
+    if (divisor <= 0 || parte < 1 || parte > 3) return ''
+
+
+
+
+    
+    /*
+    tecnologia, 3, 1
+
+    tecnologia
+    0123456789
+
+    tec nol ogia 
+    012 345 6789
+
+
+    */
 
 }
+
+
+
+
+
+
+
+// --- FUNÇÕES AUXILIARES ---
 
 // CALCULAR A MEDIANA DE UMA STRING
 function medianaString(string) {
@@ -82,4 +119,9 @@ function medianaString(string) {
         }
     }
     return mediana
+}
+
+// ARRENDONDAMENTO MANUAL
+function arrendonda(num) {
+    return num >= 0 ? num - (num % 1) : num - (num % 1) - 1
 }
