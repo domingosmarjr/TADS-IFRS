4b55797ce87c662c2587e460817a0337a81dc40e
Domingos <domingos.mar.jr@outlook.com>
2026-03-19 16:16:23 -0300

    ED-Aula03-Exercicios

---
+++ Disciplinas/4-Semestre/Estrutura-de-Dados/aula03/Exercicio2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 1) Defina uma estrutura Motor (int potencia, int modelo, double consumo)
+// 2) Inclua na estrutura Carro, um motor;
+// 3) Defina valores para os atributos de um carro e seu motor;
+// 4) Mostre os dados do carro;
+// 5) Crie uma função para trocar o consumo do motor, passe a estrutura
+// carro para função;
+// 6) Crie uma função para consultar o modelo do motor, passe a estrutura
+// carro como parâmetro, mostre o modelo do motor;
+
+int main() {
+
+}
